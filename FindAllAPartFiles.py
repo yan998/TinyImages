36133ca71f2bdcd4cb6cd459878b60aafbe0e413
zhangyan51 <zhangyan51@58.com>
2020-07-31 10:54:53 +0800

    update

--- FindAllAPartFiles.py
+++ FindAllAPartFiles.py
@@ -63,7 +63,9 @@ def checkFileInfo(findTypes, fromFilePath, exceptPathNames):
 
             for willFindName in findTypes:
                 if willFindName in fileName:
-                    finalData[willFindName].append(filePath)
+                    # 文件大小+文件路径拼接
+                    finalStr = getFilePathSize(filePath) + filePath
+                    finalData[willFindName].append(finalStr)
                     fileNum = fileNum + 1
                     break
 
@@ -85,7 +87,10 @@ def checkFileInfo(findTypes, fromFilePath, exceptPathNames):
             if fileSuffix in findTypes:
                 # 找到相关的文件，进行统计
                 filePath = root + '/' + name
-                finalData[fileSuffix].append(filePath)
+                fileSize = os.path.getsize(filePath)
+
+                finalStr = ("%dB*" % (fileSize)) + filePath
+                finalData[fileSuffix].append(finalStr)
                 fileNum = fileNum + 1
 
     return finalData
@@ -101,6 +106,50 @@ def checkSomeFileNamesIsInPath(exceptPathNames, filePath):
     return False
 
 
+# 获取文件夹存储内容大小
+def getFilePathSize(filePathStr):
+    fileSize = 0
+    for root, dirs, files in os.walk(filePathStr):
+        for name in files:
+            # os.path.join路径拼接   os.path.join(root, name) 直接获取最里面一层文件
+            # 文件的路径
+            filePath = root + '/' + name
+            # 统计文件大小
+            fileSize = fileSize + os.path.getsize(filePath)
+    return ("%dB*" % (fileSize))
+
+
+def bubbleSort(arr):
+    for i in range(1, len(arr)):
+
+        for j in range(0, len(arr) - i):
+
+            findFilePathJ1 = arr[j]
+            fileSizeIJ1 = int(findFilePathJ1.split("B*")[0])
+
+            findFilePathJ2 = arr[j + 1]
+            fileSizeJ2 = int(findFilePathJ2.split("B*")[0])
+
+            if fileSizeIJ1 > fileSizeJ2:
+                tempStr = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = tempStr
+    return arr
+
+
+def transformUnit(sizeStr):
+    size = int(sizeStr)
+    unit = "B"
+    if (size > 1000):
+        size = size / 1000
+        unit = "KB"
+        if size > 1000:
+            size = size / 1000;
+            unit = "M"
+
+    return ('%d%s' % (size, unit))
+
+
 if __name__ == '__main__':
     # 获取输入的参数
     findTypes, path, exceptPathNames = getInputParm()
@@ -112,7 +161,11 @@ if __name__ == '__main__':
     for findType in findTypes:
         list = finalData[findType]
         print("\n\n发现 %s  %d个" % (findType, len(list)))
+        list = bubbleSort(list)
         index = 1
         for findFilePath in list:
-            print("%d - %s" % (index, findFilePath))
+            # 对文件的大小进行加工
+            strList = findFilePath.split("B*")
+            sizeStr = transformUnit(strList[0])
+            print("%d - %s %s" % (index, sizeStr, strList[1]))
             index = index + 1
